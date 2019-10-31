3e682a5f709ed82221d6dff79091ecff30f06717
Chelzea Salvador <chelzeasalvador@gmail.com>
2019-10-31 15:11:59 +0800

    Nested Elements

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return <div>Hello World!!!</div>;
+    return (
+      <div>
+        <h1>Header</h1>
+        <h2>Content</h2>
+        <p>This is the content!!!</p>
+      </div>
+    );
   }
 }
 export default App;
