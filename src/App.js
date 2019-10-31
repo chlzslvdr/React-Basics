927c0dc950f936b61eddbdb46e42b8234d1a13de
Chelzea Salvador <chelzeasalvador@gmail.com>
2019-10-31 15:17:45 +0800

    Javascript Expression

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Header</h1>
-        <h2>Content</h2>
-        <p>This is the content!!!</p>
+        <h1>{1 + 1}</h1>
       </div>
     );
   }
