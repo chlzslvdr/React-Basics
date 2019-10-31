99b25c87eab5d5684e09c42e2a372ddaf83fbb02
Chelzea Salvador <chelzeasalvador@gmail.com>
2019-10-31 16:46:42 +0800

    using State

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
     this.state = {
-      data: [
-        {
-          id: 1,
-          name: 'Harry',
-          age: '12',
-        },
-        {
-          id: 2,
-          name: 'Ron',
-          age: '12',
-        },
-        {
-          id: 3,
-          name: 'Hermione',
-          age: '12',
-        },
-      ],
+      header: 'Header from state...',
+      content: 'Content from state...',
     };
   }
-
   render() {
     return (
       <div>
-        <Header />
-        <table>
-          <tbody>
-            {this.state.data.map((person, i) => (
-              <TableRow key={i} data={person} />
-            ))}
-          </tbody>
-        </table>
+        <h1>{this.state.header}</h1>
+        <h2>{this.state.content}</h2>
       </div>
     );
   }
 }
-
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Header</h1>
-      </div>
-    );
-  }
-}
-
-class TableRow extends React.Component {
-  render() {
-    return (
-      <tr>
-        <td>{this.props.data.id}</td>
-        <td>{this.props.data.name}</td>
-        <td>{this.props.data.age}</td>
-      </tr>
-    );
-  }
-}
-
 export default App;
