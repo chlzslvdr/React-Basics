995b2a1cb6e58ddffa0978c2337cdbc34cbac1ab
Chelzea Salvador <chelzeasalvador@gmail.com>
2019-10-31 17:20:05 +0800

    Calling A Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,13 @@
 import React from 'react';
+import './App.scss';
+import ParentComponent from './parentComponent';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [
-        {
-          id: 1,
-          name: 'Harry',
-          age: '12',
-        },
-        {
-          id: 2,
-          name: 'Ron',
-          age: '12',
-        },
-        {
-          id: 3,
-          name: 'Hermione',
-          age: '12',
-        },
-      ],
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <table>
-          <tbody>
-            {this.state.data.map((person, i) => (
-              <TableRow key={i} data={person} />
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
-
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Header</h1>
-      </div>
-    );
-  }
-}
-
-class TableRow extends React.Component {
-  render() {
-    return (
-      <tr>
-        <td>{this.props.data.id}</td>
-        <td>{this.props.data.name}</td>
-        <td>{this.props.data.age}</td>
-      </tr>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className='App'>
+      <ParentComponent />
+    </div>
+  );
+};
 
 export default App;
