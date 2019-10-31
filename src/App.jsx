7a66281af492581a7efc370329ac5b2666108fd8
Chelzea Salvador <chelzeasalvador@gmail.com>
2019-10-31 16:40:46 +0800

    Stateful Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        {
+          id: 1,
+          name: 'Harry',
+          age: '12',
+        },
+        {
+          id: 2,
+          name: 'Ron',
+          age: '12',
+        },
+        {
+          id: 3,
+          name: 'Hermione',
+          age: '12',
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <Content />
+        <table>
+          <tbody>
+            {this.state.data.map((person, i) => (
+              <TableRow key={i} data={person} />
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
 }
+
 class Header extends React.Component {
   render() {
     return (
@@ -19,14 +49,17 @@ class Header extends React.Component {
     );
   }
 }
-class Content extends React.Component {
+
+class TableRow extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Content</h2>
-        <p>The content text!!!</p>
-      </div>
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.name}</td>
+        <td>{this.props.data.age}</td>
+      </tr>
     );
   }
 }
+
 export default App;
