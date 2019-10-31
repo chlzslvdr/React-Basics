4ee0240cdd41ab1858fc9392cbfa91f2d57fe684
Chelzea Salvador <chelzeasalvador@gmail.com>
2019-10-31 15:20:19 +0800

    Ternary Expressions

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
+    var i = 1;
     return (
       <div>
-        <h1>{1 + 1}</h1>
+        <h1>{i == 1 ? 'True!' : 'False'}</h1>
       </div>
     );
   }
