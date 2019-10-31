4bebba18fc183e2a87f7f1ae58b3e37cd74d3e18
Chelzea Salvador <chelzeasalvador@gmail.com>
2019-10-31 20:40:16 +0800

    setState

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,32 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import './App.scss';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Initial State',
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      name: 'New State !!!',
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h3>Array: {this.props.propArray}</h3>
-        <h3>Bool: {this.props.propBool ? 'True...' : 'False...'}</h3>
-        <h3>Func: {this.props.propFunc(3)}</h3>
-        <h3>Number: {this.props.propNumber}</h3>
-        <h3>String: {this.props.propString}</h3>
-        <h3>Object: {this.props.propObject.objectName1}</h3>
-        <h3>Object: {this.props.propObject.objectName2}</h3>
-        <h3>Object: {this.props.propObject.objectName3}</h3>
+      <div className='test-app'>
+        <button onClick={this.handleClick} className='click-btn'>
+          Click Me
+        </button>
+        <span className='name'>{this.state.name}</span>
       </div>
     );
   }
 }
-
-App.propTypes = {
-  propArray: PropTypes.array.isRequired,
-  propBool: PropTypes.bool.isRequired,
-  propFunc: PropTypes.func,
-  propNumber: PropTypes.number,
-  propString: PropTypes.string,
-  propObject: PropTypes.object,
-};
-
-App.defaultProps = {
-  propArray: [1, 2, 3, 4, 5],
-  propBool: true,
-  propFunc: function(e) {
-    return e;
-  },
-  propNumber: 1,
-  propString: 'String value...',
-
-  propObject: {
-    objectName1: 'objectValue1',
-    objectName2: 'objectValue2',
-    objectName3: 'objectValue3',
-  },
-};
 
 export default App;
