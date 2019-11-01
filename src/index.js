0205a28154e7eb1980d1cfc695e08cd85e8769ca
chlzslvdr <37092603+chlzslvdr@users.noreply.github.com>
2019-11-01 23:20:26 +0800

    Revert "Forms"

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Routes from './Routes';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<Routes />, document.getElementById('root'));
 
-// setTimeout(() => {
-//   ReactDOM.unmountComponentAtNode(document.getElementById('root'));
-// }, 12000);  //for Life Cycles (to test componentWillUnmount)
+setTimeout(() => {
+  ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+}, 12000);
 
 serviceWorker.unregister();
