337f66bbf8d747386eca84187c7311dca61f4048
Chelzea Salvador <chelzeasalvador@gmail.com>
2019-11-06 15:19:16 +0800

    convert

--- src/components/02-JSX/usingJSX.jsx
+++ src/components/02-JSX/usingJSX.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-class JSX extends React.Component {
-  render() {
-    return (
-      <div className='test-app'>
-        <span className='title'>Using JSX</span>
-        <br />
-        <span className='container'>Hello World!!!</span>
-      </div>
-    );
-  }
-}
+const JSX = () => {
+  return (
+    <div className='test-app'>
+      <span className='title'>Using JSX</span>
+      <br />
+      <span className='container'>Hello World!!!</span>
+    </div>
+  );
+};
 
 export default JSX;
--- src/components/06-ComponentLifecycles/SetState/index.jsx
+++ src/components/06-ComponentLifecycles/SetState/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class SetState extends React.Component {
+class SetState extends Component {
   constructor(props) {
     super(props);
     this.state = {
